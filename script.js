0a187cd6a4a61952efb92b7b7927cfce3422bc6f
maisa abusalem <heart.webd@gmail.com>
2022-01-03 09:31:20 +0100

    Exersice 1

---
+++ script.js
@@ -0,0 +1,23 @@
+/**
+ * Exercise 1.1 (1 points)
+ *
+ * Create five variables: `card1, card2, card3, card4, card5`.
+ *
+ * Assign the values `4, 2, 7, 1, 11` to the variables created above.
+ *
+ * Add them up and put the sum in a variable called `result`.
+ *
+ * Answer with the variable `result`.
+ *
+ */
+
+ let card1 = 4;
+ let card2 = 2;
+ let card3 = 7;
+ let card4 = 1;
+ let card5 = 11;
+ console.log('card 1: ',card1 ,'card 2: ', card2,'card 3: ', card3,'card 4: ', card4,'card 5: ', card5);
+ let result = card1 + card2 + card3 + card4 + card5;
+ console.log('Result: ',result)
+ 
+ 
